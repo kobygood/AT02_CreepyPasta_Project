8a78c00ae7f4c5891b9e4bf8cce431b47c2b9b25
kobygood <kobygg888@gmail.com>
2024-09-09 15:23:08 +0800

    Removed Inventory scripts
    Too much integration

--- AT02_CreepyPasta/Assets/Scripts/Flashlight/FlashLight.cs
+++ AT02_CreepyPasta/Assets/Scripts/Flashlight/FlashLight.cs
@@ -6,24 +6,24 @@ using UnityEngine;
 
 public class Flashlight : MonoBehaviour
 {
-    Light m_Light;
+    private Light m_Light;
     public bool drainOverTime;
     public float maxBrightness;
     public float minBrightness;
     public float drainRate;
 
-    private Inventory inventory;
-
+    // Start is called before the first frame update
     void Start()
     {
         m_Light = GetComponent<Light>();
-        inventory = FindObjectOfType<Inventory>();
+        m_Light.intensity = maxBrightness; // Ensure the flashlight starts at max brightness
     }
 
+    // Update is called once per frame
     void Update()
     {
         m_Light.intensity = Mathf.Clamp(m_Light.intensity, minBrightness, maxBrightness);
-        if (drainOverTime && m_Light.enabled)
+        if (drainOverTime == true && m_Light.enabled == true)
         {
             if (m_Light.intensity > minBrightness)
             {
@@ -34,35 +34,10 @@ public class Flashlight : MonoBehaviour
         {
             m_Light.enabled = !m_Light.enabled;
         }
-        if (Input.GetKeyDown(KeyCode.R))
-        {
-            RechargeFlashlight();
-        }
     }
 
     public float GetLightIntensity()
     {
         return m_Light.intensity;
-    }
-
-     public void ToggleLight()
-        {
-            m_Light.enabled = !m_Light.enabled;
-        }
-
-    void RechargeFlashlight()
-    {
-        Item battery = inventory.GetItemByName("Battery");
-
-        if (battery != null && battery.quantity > 0)
-        {
-            m_Light.intensity = maxBrightness; 
-            inventory.UseItem(battery); 
-            Debug.Log("Flashlight Recharged");
-        }
-        else
-        {
-            Debug.Log("No batteries.");
-        }
     }
 }
--- AT02_CreepyPasta/Assets/Scripts/Inventory/ItemObject.cs
+++
@@ -1,7 +0,0 @@
-using UnityEngine;
-
-[CreateAssetMenu(fileName = "NewItem", menuName = "Inventory/Item")]
-public class ItemObject : ScriptableObject
-{
-    public Item item;
-}
